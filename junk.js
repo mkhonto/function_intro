8367728835c3980638662ecbd4c37f87306e9f86
mkhonto <jabulani@projectcodex.co>
2016-11-03 14:04:07 +0200

    function magics

---
+++ junk.js
@@ -0,0 +1,10 @@
+var multiplyPara = function(pOne, pTwo){
+  var result = pOne * pTwo;
+  return result
+};
+
+var hello = multiplyPara(3, 5); // to call the mulptiplePara function
+console.log(hello);
+
+var world = multiplyPara(7, 9); // a new function
+console.log(world);
