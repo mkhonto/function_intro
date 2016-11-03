71b583b7187cd3b2799d0eeab64b1a971a02eb0f
mkhonto <jabulani@projectcodex.co>
2016-11-03 14:24:50 +0200

    repeated variables

---
+++ repeat.js
@@ -0,0 +1,12 @@
+ // repeating variables
+var stri = "awesome string";
+console.log(stri);
+console.log(typeof stri);
+
+var numb = 45;
+console.log(numb);
+console.log(typeof numb);
+
+var boo = true;
+console.log(boo);
+console.log(typeof boo);
