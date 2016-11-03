e10f3ab906cf0428a69c35fc649fc7a34c76e8aa
mkhonto <jabulani@projectcodex.co>
2016-11-03 15:12:47 +0200

    function expression session

---
+++ function_expression.js
@@ -0,0 +1,7 @@
+// func expression load only when the interpreter reaches the line code
+
+myFun(); //call the function
+
+var myFun = function(){ //declare the function
+  console.log("function expression");
+}
