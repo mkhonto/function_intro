c205795b6024a02ed2c8cb8ca972e6c82c55bb25
mkhonto <jabulani@projectcodex.co>
2016-11-03 16:04:26 +0200

    functions building steps

---
+++ function.js
@@ -0,0 +1,13 @@
+var theFu = function(){ //this is an empty function
+  //code here
+  // return results here
+}
+
+var thePaFunction = function(me1, me2){
+  //function logic here
+  var display = me1 + me2;
+  //return(parameters)
+  return display;
+};
+// prints
+console.log(thePaFunction(1,2));
