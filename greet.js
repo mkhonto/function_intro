d889aeec41bec979657aa5876b479982363e7d99
mkhonto <jabulani@projectcodex.co>
2016-11-08 10:29:30 +0200

    split the string and get the length of it

--- greet.js
+++ greet.js
@@ -1,3 +1,4 @@
+//takes name as a param and returns a message
 const assert = require("assert");
 var greet = function(name){
 var jah = "how is family "+ name;
@@ -5,3 +6,14 @@ console.log(jah);
 return jah;
 };
 assert.equal(greet("sipho"), "how is family sipho");
+
+var countRegNumb = function(cars){
+  //var string = "CA 182736,CY 523519,CJ 812328";
+  var count = cars.split(",");
+  var count = count.length;
+
+  console.log(count);
+  return count;
+}
+var CountReg = countRegNumb("CA 182736,CY 523519,CJ 812328");
+assert.equal(CountReg, 3);
