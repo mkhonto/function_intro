1b829460264ecdb2c4984e06e76f5638358e856a
mkhonto <jabulani@projectcodex.co>
2016-11-10 11:25:35 +0200

    modified because of some errors

--- isWeekday.js
+++ isWeekday.js
@@ -1,7 +1,11 @@
 const assert = require("assert");
-const date = new Date("2020-07-12");
+//new date function is part of the built in family of javascript methods that doesn't need braces
+var date = new Date("Thu Nov 10 2016");
   weekDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 //return index between 0 and 6
   console.log(date.getDay());
+  return weekDays;
+//new Date();
 //look up the week day text string from the Array
 console.log(weekDays[date.getDay()]);
+assert.equal(new Date(weekDays), "Thu Nov 10 2016");
