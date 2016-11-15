220d8f94a921dcafd221ed250d9e705bf5bd10d5
mkhonto <jabulani@projectcodex.co>
2016-11-15 11:33:02 +0200

    modifications done

--- sameWeekday.js
+++ sameWeekday.js
@@ -1,14 +1,13 @@
 const assert = require("assert");
 //takes in two dates and check if both dates have the same weekday
-var sameDay = function(day1, day2){
-day1 = new Date(day1);
-day2 = new Date(day2);
-//console.log(day1);
+var sameDay = function(day1, day2) {
+  //day1 = new Date("2015/11/13");
+  //day2 = new Date("2016/11/11");
 var week1 = day1.getDay();
 var week2 = day2.getDay();
 
-//console.log(week1 == week2);
-  return (week1 == week2);
+console.log(week1 == week2);
+return (week1 == week2);
 };
-console.log();
-assert.equal(sameDay("2015/11/13","2016/11/11"), true);
+//console.log();
+assert.equal(sameDay(new Date("2015/11/13"), new Date("2016/11/11")), true);
