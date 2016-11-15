43aa9a4f52b71b72edae5975b501a9580fb08461
mkhonto <jabulani@projectcodex.co>
2016-11-15 11:25:30 +0200

    modifications done

--- yearsAgo.js
+++ yearsAgo.js
@@ -1,8 +1,10 @@
 const assert = require("assert");
-var call1 = new Date("2000");
-var call2 = new Date("1994");
+var yearsAgo = function(call1, call2){
+//var call1 = new Date("2000");
+//var call2 = new Date("1994");
 var news = call1.getFullYear(call1) - call2.getFullYear(call2);
 
  console.log(news);
 return news;
-assert.equal(call1, call2(news), "6");
+};
+assert.equal(yearsAgo(new Date("2000"), new Date("1994")), "6");
