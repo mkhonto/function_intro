bf8705367aa0849fb0a47c3adb4656ef1ecb0adc
mkhonto <jabulani@projectcodex.co>
2016-11-15 11:23:50 +0200

    modifications done

--- regCheck.js
+++ regCheck.js
@@ -1,22 +1,25 @@
 const assert = require("assert");
-var regCheck = function(plate, location){
+var regCheck = function(plate, location) {
   chec = location.endsWith(plate);
 
   return chec;
 }
-var isGP = regCheck("GP","123452 GP");
+var isGP = regCheck("GP", "123452 GP");
 //it shout print true
 console.log(isGP);
+assert.equal(regCheck("GP", "123452 GP"), true);
 
 var isL = regCheck("L", "234567 L");
 //it should print true
 console.log(isL);
+assert.equal(regCheck("L", "234567 L"), true);
 
 var isEC = regCheck("EC 543217", "EC");
 //it should print false
 console.log(isEC);
+assert.equal(regCheck("EC 543217", "EC"), false);
 
 var isMP = regCheck("456789 MP", "MP");
 //it should print false
 console.log(isMP);
-assert.equal(regCheck('456789 MP','MP'), false);
+assert.equal(regCheck('456789 MP', 'MP'), false);
